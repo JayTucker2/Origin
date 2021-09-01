58a5ac0cc22143b760aae212af73afaa38e5aa23
JayTucker2 <prosnipa1221@gmail.com>
2021-09-01 08:06:42 -0500

    Initial Commit

--- TextField/ViewController.swift
+++ TextField/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var ageTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func click(_ sender: UIButton) {
+       let theName = nameTextField.text!
+        
+        let theAge = ageTextField.text!
+        label.text = "Name: \(theName) Age: \(theAge)"
+        
+    }
+    
 }
+    
+
 
